c355eacec5d0976fb5b3c01028d2ba29f2deb9c1
voordine <nadinekraakman@yahoo.com>
2023-10-10 12:04:47 +0200

    Add project files.

---
+++ PR 4 Silhouet/Program.cs
@@ -0,0 +1,20 @@
+﻿class hi
+{
+    public static void Main()
+    {
+        string[] ninput = Console.ReadLine().Split(' ');
+        int n = int.Parse(ninput[0]);
+
+        int i = 0;
+        while (i < n)
+        {
+            string[] gebouwinput = Console.ReadLine().Split(' ');
+            int l = int.Parse(gebouwinput[0]);
+            int h = int.Parse(gebouwinput[1]);
+            int r = int.Parse(gebouwinput[2]);
+            i++;
+        }
+
+        Console.WriteLine();
+    }
+}
