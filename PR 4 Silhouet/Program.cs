4942ae65c35d30db357e61a46fee242bce2fed13
voordine <nadinekraakman@yahoo.com>
2023-10-13 22:03:55 +0200

    dit is i e t s

--- PR 4 Silhouet/Program.cs
+++ PR 4 Silhouet/Program.cs
@@ -27,63 +27,66 @@ public class strook
 
 public static class hi
 {
-    public static List<strook> merge(List<gebouw> a, int p, int q, int r)
+    public static List<strook> merge(List<strook> a, List<strook> b)
     {
-        int nl = q - p + 1;
-        int nr = r - q;
-        gebouw[] L = new gebouw[nl-1];
-        gebouw[] R = new gebouw[nr-1];
-        int i, j, k;
-        k = p;
+        int i = 0, j = 0;
+        int h1 = 0, h2 = 0;
+        int maxh = 0;
         List<strook> merged = new List<strook>();
 
-        for (i = 0; i < nl; ++i) { L[i] = a[p+i]; }
-        for (j = 0; j < nr; ++j) { R[j] = a[p+j+1]; }
-
-        i = 0;
-        j = 0;
-
-        while (i < nl && j < nr) 
+        while (i < a.Count && j < b.Count) 
         { 
-            if (L[i].links <= R[j].links)
+            if (a[i].x < b[j].x)
             {
-                a[k] = L[i];
+                h1 = a[i].h;
+                maxh = Math.Max(h1, h2);
+                merged.Add(new strook(a[i].x, maxh));
                 i++;
             }
-            else
+            else if (a[i].x > b[j].x)
             {
-                a[k] = R[j];
+                h2 = b[j].h;
+                maxh = Math.Max(h1, h2);
+                merged.Add(new strook(b[j].x, maxh));
                 j++;
             }
-            k++;
+            else  
+            {
+                maxh = Math.Max(a[i].h, b[j].h);
+                i++;
+                j++;
+            }
+
+            if (merged.Count == 0 || maxh != merged[merged.Count-1].h)
+            {
+                merged.Add(new strook(a[i].x, maxh));
+            }
         }
-        while (i < nl)
+        while (i < a.Count)
         {
-            a[k] = L[i];
+            merged.Add(a[i]);
             i++;
-            k++;
         }
-        while (j < nr)
+        while (j < b.Count)
         {
-            a[k] = R[j];
+            merged.Add(b[j]);
             j++;
-            k++;
         }
         return merged;
     }
      
     public static List<strook> mergesort(List<gebouw> a, int p, int r) 
     {
-        if (a.Count == 1)
+        if (p == r)
         {
             strook strk1 = new strook(a[0].links, a[0].hoogte);
             strook strk2 = new strook(a[0].rechts, 0);
             return new List<strook> { strk1, strk2 };
         }
         int q = (p+r)/2;
-        mergesort(a, p, q);
-        mergesort(a, q + 1, r);
-        return merge(a, p, q, r);
+        List<strook> left = mergesort(a, p, q);
+        List<strook> right = mergesort(a, q + 1, r);
+        return merge(left, right);
     }
 
     public static void Main()
@@ -102,9 +105,9 @@ public static class hi
             gebouwen.Add(new gebouw(l, h, r));
             i++;
         }
-        List<strook> result = mergesort(gebouwen, 0, gebouwen.Count - 1);
-        Console.WriteLine(result.Count);
-        foreach (var strook in result) 
+        List<strook> stroken = mergesort(gebouwen, 0, gebouwen.Count - 1);
+        Console.WriteLine(stroken.Count);
+        foreach (var strook in stroken) 
         {
             Console.WriteLine($"{strook.x} {strook.h}");
         }
